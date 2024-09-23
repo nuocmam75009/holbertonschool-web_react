5707d1c06eff0ebe2ac1c2c683c0ab2d37030b05
nuocmam75009 <sayhi.kiseiju@gmail.com>
2024-09-23 10:56:10 +0200

    modif file

---
+++ TypeScript/task_0/js/main.ts
@@ -0,0 +1,27 @@
+// Init: tsc --init
+
+interface Student {
+    firstName : string,
+    lastName : string,
+    age : number,
+    location : string
+};
+
+const student1: Student = {
+    firstName: "John",
+    lastName: "Doe",
+    age: 20,
+    location: "New York"
+};
+
+const student2: Student = {
+    firstName: "Jane",
+    lastName: "Smith",
+    age: 22,
+    location: "Los Angeles"
+};
+
+let studentList: Student[] = [student1, student2];
+
+const table = document.createElement('table');
+document.body.appendChild(table);
